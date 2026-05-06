9da751ec8f26859b5517a92586a95f2156a7d6cc
skillbridgearabia-ai <skillbridgearabia@gmail.com>
2026-05-06 18:09:07 +0300

    Refactor routes and add sitemap and robots files

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const path    = require('path');
-const fs      = require('fs');
 const app     = express();
 
-app.get('/investor', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'investor.html'));
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+app.get('/investor',    (req, res) => res.sendFile(path.join(__dirname, 'public', 'investor.html')));
+app.get('/sitemap.xml', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sitemap.xml')));
+app.get('/robots.txt',  (req, res) => res.sendFile(path.join(__dirname, 'public', 'robots.txt')));
+app.get('/',            (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('*',            (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('SkillBridge Arabia on port ' + PORT));
